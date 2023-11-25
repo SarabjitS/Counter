f505367ba4c0e28e0b2ccedb4744b4fabcb23356
SarabjitS <35809229+SarabjitS@users.noreply.github.com>
2023-11-25 16:01:31 -0330

    reduce variable

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const decreaseButtonEl = document.querySelector(".counter__button--decrease");
 const resetButtonEl = document.querySelector(".counter__reset-button");
 const counterValueEl = document.querySelector(".counter__value");
 const counterTitleEl = document.querySelector(".counter__title");
+const currentValue = counterValueEl.textContent;
+// convert value value to number type
+let currentValueAsNumber = +currentValue;
 
 resetButtonEl.addEventListener("click", () => {
   // set counter value to 0
@@ -24,42 +27,28 @@ resetButtonEl.addEventListener("click", () => {
 });
 
 const decreaseCounter = () => {
-  // get current value of counter
-  const currentValue = counterValueEl.textContent;
-
-  // convert value value to number type
-  const currentValueAsNumber = +currentValue;
-
-  // decrement by 1
-  let newValue = currentValueAsNumber - 1;
+  currentValueAsNumber--;
 
   // check if new value is less than 0
-  if (newValue < 0) {
+  if (currentValueAsNumber < 0) {
     // if it is, force it to be 0 instead
-    newValue = 0;
+    currentValueAsNumber = 0;
   }
 
   // update counter value with new value
-  counterValueEl.textContent = newValue;
+  counterValueEl.textContent = currentValueAsNumber;
 
   // unfocus (blur) button
   decreaseButtonEl.blur();
 };
 
 const incrementCounter = () => {
-  // get current value of counter
-  const currentValue = counterValueEl.textContent;
-
-  // convert value to number type
-  const currentValueAsNumber = +currentValue;
-
-  // increment by 1
-  let newValue = currentValueAsNumber + 1;
+  currentValueAsNumber++;
 
   // check if new value is greater than 5
-  if (newValue > 5) {
+  if (currentValueAsNumber > 5) {
     // if it is, force it to be 5 instead
-    newValue = 5;
+    currentValueAsNumber = 5;
 
     // give visual indicator that limit has been reached
     counterEl.classList.add("counter--limit");
@@ -73,7 +62,7 @@ const incrementCounter = () => {
   }
 
   // set counter element with new value
-  counterValueEl.textContent = newValue;
+  counterValueEl.textContent = currentValueAsNumber;
 
   // unfocus (blur) button
   increaseButtonEl.blur();
@@ -83,10 +72,10 @@ increaseButtonEl.addEventListener("click", incrementCounter);
 decreaseButtonEl.addEventListener("click", decreaseCounter);
 document.addEventListener("keydown", function (e) {
   console.log(e.key);
-  if (e.key == "ArrowUp") {
+  if (e.key == "ArrowUp" && increaseButtonEl.disabled == false) {
     incrementCounter();
   }
-  if (e.key == "ArrowDown") {
+  if (e.key == "ArrowDown" && decreaseButtonEl.disabled == false) {
     decreaseCounter();
   }
 });
