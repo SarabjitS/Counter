fd015d3bd5f19f799e74a13b935d69e4ce2a0ac8
SarabjitS <35809229+SarabjitS@users.noreply.github.com>
2023-11-25 15:34:25 -0330

    add decrease button functionality

--- script.js
+++ script.js
@@ -5,6 +5,47 @@ const resetButtonEl = document.querySelector(".counter__reset-button");
 const counterValueEl = document.querySelector(".counter__value");
 const counterTitleEl = document.querySelector(".counter__title");
 
+resetButtonEl.addEventListener("click", () => {
+  // set counter value to 0
+  counterValueEl.textContent = 0;
+
+  // reset background color
+  counterEl.classList.remove("counter--limit");
+
+  // reset counter title
+  counterTitleEl.textContent = "Fancy Counter";
+
+  // enable increase and decrease buttons
+  increaseButtonEl.disabled = false;
+  decreaseButtonEl.disabled = false;
+
+  // unfocus (blur) reset button
+  resetButtonEl.blur();
+});
+
+decreaseButtonEl.addEventListener("click", () => {
+  // get current value of counter
+  const currentValue = counterValueEl.textContent;
+
+  // convert value value to number type
+  const currentValueAsNumber = +currentValue;
+
+  // decrement by 1
+  let newValue = currentValueAsNumber - 1;
+
+  // check if new value is less than 0
+  if (newValue < 0) {
+    // if it is, force it to be 0 instead
+    newValue = 0;
+  }
+
+  // update counter value with new value
+  counterValueEl.textContent = newValue;
+
+  // unfocus (blur) button
+  decreaseButtonEl.blur();
+});
+
 const incrementCounter = () => {
   // get current value of counter
   const currentValue = counterValueEl.textContent;
@@ -37,3 +78,6 @@ const incrementCounter = () => {
   // unfocus (blur) button
   increaseButtonEl.blur();
 };
+
+increaseButtonEl.addEventListener("click", incrementCounter);
+document.addEventListener("keydown", incrementCounter);
